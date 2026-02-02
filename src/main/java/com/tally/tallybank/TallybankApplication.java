d581fc34fd92ec52439cc5bd5249cd1962cef859
T Z Nkgau <nkgautz@gmail.com>
2026-02-03 01:17:18 +0200

    connected to database

---
+++ src/main/java/com/tally/tallybank/TallybankApplication.java
@@ -0,0 +1,13 @@
+package com.tally.tallybank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TallybankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TallybankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tally/tallybank/TallybankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tally.tallybank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TallybankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
